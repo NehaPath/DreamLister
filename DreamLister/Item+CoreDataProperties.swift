ccb3b72aa3175ef908506fc567e08a82e1c21f15
Neha Pathmanaban <neha.pathmanaban@gmail.com>
2017-03-13 21:43:39 -0400

    navigation and data persistence

--- DreamLister/Item+CoreDataProperties.swift
+++ DreamLister/Item+CoreDataProperties.swift
@@ -25,3 +25,6 @@ extension Item {
     @NSManaged public var toStore: Store?
 
 }
+
+
+
---
+++ DreamLister/ItemsDetailsVC.swift
@@ -0,0 +1,40 @@
+//
+//  ItemsDetailsVC.swift
+//  DreamLister
+//
+//  Created by Neha Pathmanaban on 3/13/17.
+//  Copyright © 2017 Neha Pathmanaban. All rights reserved.
+//
+
+import UIKit
+
+class ItemsDetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let topItem = self.navigationController?.navigationBar.topItem {
+            
+            topItem.backBarButtonItem = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.plain, target: nil, action: nil)
+        }
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
